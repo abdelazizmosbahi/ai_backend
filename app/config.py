9b89f2783b3c32159996cd500bf56d879357a0d2
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-11 14:11:59 +0100

    Update backend to use .env for configuration

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Configuration class for the Flask application
 class Config:
     # MongoDB URI, defaults to local AskSphere database if not set in .env
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/asksphere')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://mosbehiasiz:BSvss3YfLyb0ojMa@cluster0.ntfhykc.mongodb.net/asksphere?retryWrites=true&w=majority')
     # Secret key for Flask, defaults to a secure value if not set in .env
     SECRET_KEY = os.getenv('SECRET_KEY', '9035aa297aca23fca3b5f070fe909e01567739b99fa41a55bb6ad63076a0adf9')
     # Azure Cognitive Services endpoint and key (placeholders for chatbot integration)

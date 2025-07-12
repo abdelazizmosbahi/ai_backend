4c56d8fe0131fc7f01f400cb226c66d2167e9991
MOSBAHI Abdelaziz <mosbehiasiz@gmail.com>
2025-07-12 13:46:32 +0100

    Secure config.py by using only .env for environment variables

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-       SECRET_KEY = os.getenv('SECRET_KEY', '79da288f9993304133b20b3d2bffaa03c59055605dbf3ac9383e1fb0a151a245')
-       MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://mosbehiasiz:BSvss3YfLyb0ojMa@cluster0.ntfhykc.mongodb.net/asksphere?retryWrites=true&w=majority')
-       AZURE_LANGUAGE_ENDPOINT = os.getenv('AZURE_LANGUAGE_ENDPOINT', '')
-       AZURE_LANGUAGE_KEY = os.getenv('AZURE_LANGUAGE_KEY', '')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MONGO_URI = os.getenv('MONGO_URI')
+    AZURE_LANGUAGE_ENDPOINT = os.getenv('AZURE_LANGUAGE_ENDPOINT')
+    AZURE_LANGUAGE_KEY = os.getenv('AZURE_LANGUAGE_KEY')
